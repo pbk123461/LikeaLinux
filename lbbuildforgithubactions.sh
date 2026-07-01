513400af6629ae795bc8571100733f3425d87134
pbk123461 <118950736+pbk123461@users.noreply.github.com>
2026-07-01 17:18:45 -0400

    Update build script references in buildarm64.yaml and add lbbuildforgithubactions.sh

---
+++ lbbuildforgithubactions.sh
@@ -0,0 +1,17 @@
+lb config \
+  --distribution trixie \
+  -a arm64 \
+  --apt-recommends true \
+  --compression zstd \
+  --mirror-binary https://deb.debian.org/debian/ \
+  --mirror-bootstrap https://deb.debian.org/debian/ \
+  --linux-flavours "arm64" \
+  --archive-areas "main contrib non-free non-free-firmware" \
+  --iso-application "KazumaKiryu" \
+  --iso-publisher "pbk123461" \
+  --iso-volume "LIKEALINUX0" \
+  --binary-images iso-hybrid \
+  --system live \
+  --bootappend-live "boot=live components quiet splash username=kiryulive hostname=LikeALinux" \
+  --hdd-label "DojimaHQ" \
+  --build-with-chroot true
